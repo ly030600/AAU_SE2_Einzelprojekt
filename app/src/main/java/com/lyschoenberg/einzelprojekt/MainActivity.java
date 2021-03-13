8c731983b79c2c11f60fa460968e8a3ff933fccb
Lydia Schönberg <lyschoenberg@edu.aau.at>
2021-03-13 21:39:36 +0100

    Layout Anpassungen

--- app/src/main/java/com/lyschoenberg/einzelprojekt/MainActivity.java
+++ app/src/main/java/com/lyschoenberg/einzelprojekt/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNumber;
+    private TextView labelTextView;
     private TextView answerTextView;
 
     @Override
@@ -16,17 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextNumber = findViewById(R.id.editTextNumber);
+        labelTextView = findViewById(R.id.labelTextView);
         answerTextView = findViewById(R.id.answerTextView);
     }
 
     public void sendMessage(View view) {
         String matrikelnummer = editTextNumber.getText().toString();
+        labelTextView.setText("Antwort vom Server");
         TcpClientThread thread = new TcpClientThread(answerTextView, matrikelnummer);
         thread.start();
     }
 
     public void calculatePrimzahl(View view) {
         String matrikelnummer = editTextNumber.getText().toString();
+        labelTextView.setText("Berechnung Primzahl");
         PrimzahlThread thread = new PrimzahlThread(answerTextView, matrikelnummer);
         thread.start();
     }
